e947861dce5a5c1273e753063b3dfd64f2941c2b
chandmouli <30821979+chandmouli@users.noreply.github.com>
2017-08-08 11:50:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function(req,send){
-res.send('article one will appear here');
+res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two',function(req,send){
 res.send('article two will appear here');
